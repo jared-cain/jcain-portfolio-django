ce97d0adeea695f9e2521d2e6c8277ce98b146eb
jared <jared>
2016-01-13 16:58:55 -0800

    Add templates folder and setup home view

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
 # Create your views here.
+
+def index(request):
+    template = 'index.html'
+    current_user = ''
+
+    if request.user.is_authenticated():
+        current_user = request.user.__str__
+
+    context = {
+        'user': current_user
+    }
+
+    return render(request, template, context)
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from accounts.views import index
 
 urlpatterns = [
+    url(r'^$', index, name='home'),
     url(r'^admin/', admin.site.urls),
 ]
