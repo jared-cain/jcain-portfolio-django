39e9d7eb0978419c9c09a0cc3a53934d6f98eddf
Jared Cain <jared.cain77@gmail.com>
2016-01-18 17:29:56 -0800

    Add py_query set to views and update index and css

--- portfolio/views.py
+++ portfolio/views.py
@@ -4,12 +4,15 @@ def index(request):
     template = 'index.html'
     current_user = ''
 
+    python_queryset = ['Django REST Framework', 'Familiarity with STD library', 'Custom Authentication/Authorization', '3rd party libraries e.g Virtualenv, Virtualenvwrapper, crispy_forms, django-registration-redux etc.',]
+
     if request.user.is_authenticated():
         current_user = request.user.get_full_name()
         name,domain = current_user.split('@')
 
     context = {
-        'username': name
+        'username': name,
+        'py_query': python_queryset
     }
 
     return render(request, template, context)
