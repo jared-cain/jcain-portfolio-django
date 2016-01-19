d6cf012711387a9f106ab2f576d15222f4c247b6
Jared Cain <jared.cain77@gmail.com>
2016-01-18 18:49:23 -0800

    Update UI/UX and add bg to os area

--- portfolio/views.py
+++ portfolio/views.py
@@ -4,7 +4,9 @@ def index(request):
     template = 'index.html'
     current_user = ''
 
-    python_queryset = ['Django REST Framework', 'Familiarity with STD library', 'Custom Authentication/Authorization', '3rd party libraries e.g Virtualenv, Virtualenvwrapper, crispy_forms, django-registration-redux etc.',]
+    python_queryset = ['Django REST Framework', 'Knowledgable with Python STD library', 'Custom Authentication/Authorization', '3rd party libraries e.g Virtualenv, Virtualenvwrapper, crispy_forms, django-registration-redux etc.',]
+
+    ui_queryset = ['HTML5/CSS3 ', 'Bootstrap 3+', 'Javascript and libraries e.g AngularJS and Jquery', 'Font-icons and 3rd party iconic fonts e.g FontAwesome']
 
     if request.user.is_authenticated():
         current_user = request.user.get_full_name()
@@ -12,7 +14,8 @@ def index(request):
 
     context = {
         'username': name,
-        'py_query': python_queryset
+        'py_query': python_queryset,
+        'ui_query': ui_queryset
     }
 
     return render(request, template, context)
