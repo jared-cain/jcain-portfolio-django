16fbeb0a53dc8e01350684b980deb0e18d165f60
Jared Cain <jared.cain77@gmail.com>
2016-01-18 19:46:38 -0800

    Update index to include ui/ux css and templates

--- portfolio/views.py
+++ portfolio/views.py
@@ -6,7 +6,9 @@ def index(request):
 
     python_queryset = ['Django REST Framework', 'Knowledgable with Python STD library', 'Custom Authentication/Authorization', '3rd party libraries e.g Virtualenv, Virtualenvwrapper, crispy_forms, django-registration-redux etc.',]
 
-    ui_queryset = ['HTML5/CSS3 ', 'Bootstrap 3+', 'Javascript and libraries e.g AngularJS and Jquery', 'Font-icons and 3rd party iconic fonts e.g FontAwesome']
+    ui_queryset = ['HTML5/CSS3 ', 'Bootstrap 3+', 'Javascript and libraries e.g AngularJS and Jquery', 'Font-icons and 3rd party iconic fonts e.g FontAwesome', '2-Way data binding and loose coupling with AngularJS']
+
+    osx_queryset = ['Intermediate knowledge of Ubuntu Linux/BASH', 'SSH VirtualBox and Vagrant', 'Postgresql/MySQL/sqlite3', 'Version control with Git', 'VIM!']
 
     if request.user.is_authenticated():
         current_user = request.user.get_full_name()
@@ -15,7 +17,8 @@ def index(request):
     context = {
         'username': name,
         'py_query': python_queryset,
-        'ui_query': ui_queryset
+        'ui_query': ui_queryset,
+        'osx_query': osx_queryset
     }
 
     return render(request, template, context)
