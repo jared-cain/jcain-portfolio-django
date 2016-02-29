f076219d724895000e6880a41d7677836dc17fd3
Jared Cain <jared.cain77@gmail.com>
2016-02-28 23:10:47 -0800

    update about section

--- static_in_pro/our_static/js/custom.js
+++ static_in_pro/our_static/js/custom.js
@@ -127,6 +127,40 @@ jQuery.noConflict();
     });
 
 
+    function isScrolledIntoView(elem) {
+        var $elem = $(elem);
+        var $window = $(window);
+
+        var docViewTop = $window.scrollTop();
+        var docViewBottom = docViewTop + $window.height();
+
+        var elemTop = $elem.offset().top;
+        var elemBottom = elemTop + $elem.height();
+
+        // return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+        return ((( elemTop >= docViewTop) && (elemTop <= docViewBottom)) || ((elemBottom >= docViewTop) && (elemBottom <= docViewBottom)));
+    };
+
+    var aboutElem = document.getElementsByClassName("about_section");
+
+    $( document ).on('scroll', function( event ){
+        if ( isScrolledIntoView( aboutElem ) == true) {
+            console.log("You're over about!");
+
+            var $aboutElem = $( aboutElem );
+
+            $aboutElem.stop().animate({
+                marginLeft: "+=1000",
+                opacity: 1
+            },{
+                queue:true,
+                duration: 1000
+            })
+
+            $( this ).off('scroll');
+        };
+    })
+
 
 })( jQuery );
 
